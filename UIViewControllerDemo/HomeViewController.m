693e56b28532b047cac8c5cb305304a2a6126684
张诗健 <zsjcwy_xcode@163.com>
2018-03-12 17:13:09 +0800

    commit

--- UIViewControllerDemo/HomeViewController.m
+++ UIViewControllerDemo/HomeViewController.m
@@ -35,7 +35,8 @@
     self.dataSource = @[@{@"title":@"实现一个自定义容器视图控制器",@"target":@"CustomContainerViewController"},
                         @{@"title":@"子视图控制器之间的转场动画",@"target":@"TransitionContainerViewController"},
                         @{@"title":@"呈现一个视图控制器",@"target":@"PresentingViewController"},
-                        @{@"title":@"自定义转场动画",@"target":@"CustomTransitionDemoViewController"}];
+                        @{@"title":@"自定义转场动画",@"target":@"CustomTransitionDemoViewController"},
+                        @{@"title":@"自定义Presentation Controller",@"target":@"CustomPresentationViewController"}];
 }
 
 
