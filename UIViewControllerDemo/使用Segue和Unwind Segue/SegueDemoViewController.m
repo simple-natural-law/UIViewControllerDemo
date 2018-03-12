9e26b500ac834d2b5a4887cb7e8f11e5ca498aa3
张诗健 <zsjcwy_xcode@163.com>
2018-03-12 16:56:23 +0800

    commit

---
+++ UIViewControllerDemo/使用Segue和Unwind Segue/SegueDemoViewController.m
@@ -0,0 +1,41 @@
+//
+//  SegueDemoViewController.m
+//  UIViewControllerDemo
+//
+//  Created by 讯心科技 on 2018/3/12.
+//  Copyright © 2018年 讯心科技. All rights reserved.
+//
+
+#import "SegueDemoViewController.h"
+
+@interface SegueDemoViewController ()
+
+@end
+
+@implementation SegueDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIBarButtonItem * backButtonItem = [[UIBarButtonItem alloc] init];
+    backButtonItem.title = @"返回";
+    self.navigationItem.backBarButtonItem = backButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
